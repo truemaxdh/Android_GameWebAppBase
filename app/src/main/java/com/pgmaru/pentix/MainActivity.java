4139b6e5c1b6a21545494845dfa1446efc839114
maxdh <truemaxdh@gmail.com>
2020-05-09 20:18:04 +0900

    Initial commit

---
+++ app/src/main/java/com/pgmaru/pentix/MainActivity.java
@@ -0,0 +1,176 @@
+package com.pgmaru.pentix;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.games.AchievementsClient;
+import com.google.android.gms.games.EventsClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.LeaderboardsClient;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.games.PlayersClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+    WebAppInterface webAppInterface;
+
+    // Client used to sign in with Google APIs
+    public GoogleSignInClient mGoogleSignInClient = null;
+
+    // Client variables
+    public AchievementsClient mAchievementsClient;
+    public LeaderboardsClient mLeaderboardsClient;
+    public EventsClient mEventsClient;
+    public PlayersClient mPlayersClient;
+
+    public String displayName;
+
+    private static final int RC_UNUSED = 5001;
+    private static final int RC_SIGN_IN = 9001;
+
+    public InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webAppInterface = new WebAppInterface(this);
+        webView.addJavascriptInterface(webAppInterface, "Android");
+
+        // url
+        webView.loadUrl("https://truemaxdh.github.io/EnjoyCoding/game_pentix/www/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    // Back button activity
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void onConnected(GoogleSignInAccount googleSignInAccount) {
+        mAchievementsClient = Games.getAchievementsClient(this, googleSignInAccount);
+        mLeaderboardsClient = Games.getLeaderboardsClient(this, googleSignInAccount);
+        mEventsClient = Games.getEventsClient(this, googleSignInAccount);
+        mPlayersClient = Games.getPlayersClient(this, googleSignInAccount);
+
+        // Set the greeting appropriately on main menu
+        mPlayersClient.getCurrentPlayer()
+                .addOnCompleteListener(new OnCompleteListener<Player>() {
+                    @Override
+                    public void onComplete(Task<Player> task) {
+                        if (task.isSuccessful()) {
+                            displayName = task.getResult().getDisplayName();
+                        } else {
+                            displayName = "???";
+                        }
+                        //Toast.makeText(getApplicationContext(), "Hello," + displayName, Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void onDisconnected() {
+        mAchievementsClient = null;
+        mLeaderboardsClient = null;
+        mPlayersClient = null;
+        //Toast.makeText(this, "GoodBye!", Toast.LENGTH_LONG).show();
+    }
+
+    public void signInSilently() {
+        //Toast.makeText(this, "signInSilently", Toast.LENGTH_LONG).show();
+        mGoogleSignInClient.silentSignIn().addOnCompleteListener((Executor) this,
+                new OnCompleteListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onComplete(Task<GoogleSignInAccount> task) {
+                        if (task.isSuccessful()) {
+                            onConnected(task.getResult());
+                        } else {
+                            onDisconnected();
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Since the state of the signed in user can change when the activity is not active
+        // it is recommended to try and sign in silently from when the app resumes.
+        //Toast.makeText(this, "OnResume", Toast.LENGTH_LONG).show();
+        if (mGoogleSignInClient != null) {
+            signInSilently();
+        }
+    }
+
+    public void startSignInIntent() {
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), RC_SIGN_IN);
+    }
+
+    public void showAchievements() {
+        /*if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "The interstitial wasn't loaded yet.", Toast.LENGTH_LONG);
+        }*/
+        mAchievementsClient.getAchievementsIntent()
+            .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                @Override
+                public void onSuccess(Intent intent) {
+                    startActivityForResult(intent, RC_UNUSED);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                    //Toast.makeText(getApplicationContext(), "Failed to get Achievements.", Toast.LENGTH_LONG);
+                }
+            });
+    }
+
+    public void showLeaderboard() {
+        /*if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "The interstitial wasn't loaded yet.", Toast.LENGTH_LONG);
+        }*/
+        mLeaderboardsClient.getAllLeaderboardsIntent()
+            .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                @Override
+                public void onSuccess(Intent intent) {
+                    startActivityForResult(intent, RC_UNUSED);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                    //Toast.makeText(getApplicationContext(), "Failed to get Leaderboard.", Toast.LENGTH_LONG);
+                }
+            });
+    }
+}
---
+++ app/src/main/java/com/pgmaru/pentix/WebAppInterface.java
@@ -0,0 +1,80 @@
+package com.pgmaru.pentix;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import static com.google.android.gms.ads.MobileAds.initialize;
+
+public class WebAppInterface {
+    Context mContext;
+    MainActivity mMain;
+
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c) {
+        mContext = c;
+        mMain = (MainActivity)c;
+    }
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    /** AdMob Init */
+    @JavascriptInterface
+    public void adMobInit() {
+        initialize(mContext);
+        mMain.mInterstitialAd = new InterstitialAd(mContext);
+        mMain.mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); // Test
+        //ca-app-pub-7307479428475282~3962043059 : Real
+
+    }
+
+    /** AdMob Load InterstitialAd */
+    @JavascriptInterface
+    public void adMobInterstitialLoad() {
+        mMain.mInterstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+
+    /** AdMob Show InterstitialAd */
+    @JavascriptInterface
+    public void adMobInterstitialShow() {
+        if (mMain.mInterstitialAd.isLoaded()) {
+            mMain.mInterstitialAd.show();
+        } else {
+            showToast("The interstitial wasn't loaded yet.");
+        }
+    }
+
+    /** Signin to google services */
+    public void signInToGS() {
+        // Create the client used to sign in to Google services.
+        if (mMain.mGoogleSignInClient == null) {
+            mMain.mGoogleSignInClient = GoogleSignIn.getClient(mContext,
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).build());
+        }
+
+        if (GoogleSignIn.getLastSignedInAccount(mContext) == null)
+            mMain.startSignInIntent();
+    }
+
+    /** Show achievements from the web page */
+    @JavascriptInterface
+    public void showAchievements() {
+        mMain.showAchievements();
+    }
+
+    /** Show leaderboard from the web page */
+    @JavascriptInterface
+    public void showLeaderboard() {
+        mMain.showLeaderboard();
+    }
+
+}
